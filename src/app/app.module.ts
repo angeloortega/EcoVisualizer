4a1748903650bf0b05faba6beb68a5ba39aa3df2
Angelo Ortega <angeloale1@gmail.com>
2020-07-06 23:17:01 -0600

    [NEW] Dependency setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HighchartsChartModule } from "highcharts-angular";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    HighchartsChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
